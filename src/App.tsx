5b30cafc24d5c2a670e5b442a505461e202bfb8c
Arturo <arturo.aoh.26@gmail.com>
2024-03-08 13:56:54 -0600

    onesignal added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 import AppNavigator from './navigation';
 
 export default function App() {
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  OneSignal.initialize('ONESIGNAL_APP_ID');
+
+  // Method for listening for notification clicks
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
   return <AppNavigator />;
 }
