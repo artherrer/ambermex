f969ff86827bff8e6948d68c1ddd8951abf97532
Arturo <arturo.aoh.26@gmail.com>
2024-03-08 16:16:57 -0600

    onesignal app id added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import AppNavigator from './navigation';
 export default function App() {
   OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
-  OneSignal.initialize('ONESIGNAL_APP_ID');
+  OneSignal.initialize('392cd87d-017d-4303-81e7-1882b7117477');
 
   // Method for listening for notification clicks
   OneSignal.Notifications.addEventListener('click', event => {
